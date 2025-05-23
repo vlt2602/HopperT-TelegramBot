1dcdc7762004d11c4063105a21e588d33bd0123c
vlt2602 <155764244+vlt2602@users.noreply.github.com>
2025-05-23 20:10:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import nest_asyncio
 import asyncio
 from telegram_handler import start_telegram_bot
 
-if __name__ == "__main__":
-    asyncio.run(start_telegram_bot())
+nest_asyncio.apply()
+asyncio.get_event_loop().run_until_complete(start_telegram_bot())
