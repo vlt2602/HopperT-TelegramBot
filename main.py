9e1803f750f0fa9e13dbac932b9266f535411f37
vlt2602 <155764244+vlt2602@users.noreply.github.com>
2025-05-25 00:50:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from telegram_handler import start_telegram_bot
 # Kích hoạt hỗ trợ vòng lặp lồng nhau
 nest_asyncio.apply()
 
-# Khởi chạy bot Telegram
-asyncio.get_event_loop().run_until_complete(start_telegram_bot())
+if __name__ == "__main__":
+    asyncio.run(start_telegram_bot())
